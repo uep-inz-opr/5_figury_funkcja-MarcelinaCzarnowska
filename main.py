a1d9812a91e6459ca823986b178b36c4cd381b47
Marcelina <86987@student.ue.poznan.pl>
2022-05-29 10:06:24 +0200

    5 podejście

---
+++ main.py
@@ -0,0 +1,34 @@
+from math import pi
+lst = []
+figura = []
+ilosc_figur = int(float(input().strip()))
+for i in range(liczba_figur):
+    lst.append(input().split())
+
+def obliczanie_kola(a):
+        return pi * a * a
+def obliczanie_prostokata(a,b):
+        return a * b
+def obliczanie_trojkata(a,b,c):
+        return (s*(s-a)*(s-b)*(s-c)) ** 0.5
+
+pole = 0
+
+for figura in lst:
+    if len(figura) == 1:
+        a = float(figura[0])
+        pole += obliczanie_kola(a)
+    elif len(figura) == 2:
+        a = float(figura[0])
+        b = float(figura[1])
+        pole += obliczanie_prostokata(a,b)
+    elif len(figura) == 3:
+        a = float(figura[0])
+        b = float(figura[1])
+        c = float(figura[2])
+        s = ((a + b + c)/2)
+        pole += obliczanie_trojkata(a,b,c)
+    else:
+        print()
+        break
+print(round(pole,2))
